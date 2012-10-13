6fe0be6d8f6a96cd4de3643692c434b08a719d6a
Dexter Liu <dexter.h.liu@gmail.com>
2012-10-13 14:50:18 -0700

    first commit of simpleform

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = 'aa101dbd0375bad21b37f136c83f7b070825237e06294db6cbba2c652aec50b9f83ccd5e33241d8ab9255bc426fda08d5dcfb9d3d3fad96a4ea7752d11a3a389'
